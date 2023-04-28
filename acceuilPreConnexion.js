130a8344b6beabf60e8ca167df0090eba375c235
Vasilii <franco.olivier@yahoo.fr>
2023-04-28 13:43:58 +0200

    page acceuil terminée, a modifier avec organisateur et entraineur

---
+++ acceuilPreConnexion.js
@@ -0,0 +1,41 @@
+const displayform = pE('displayForm');
+const forLogin = pE('forLogin');
+const loginForm = pE('loginForm');
+const forRegister = pE('forRegister');
+const registerForm = pE('registerForm');
+const formContainer = pE('formContainer');
+displayform.addEventListener('click', showForm);
+
+forLogin.addEventListener('click', () => {
+    forLogin.classList.add('active');
+    forRegister.classList.remove('active');
+    if(loginForm.classList.contains('toggleForm'))
+    {
+        formContainer.style.transform = 'translate(0%)';
+        formContainer.style.transition = 'transform .5s';
+        registerForm.classList.add('toggleForm');
+        loginForm.classList.remove('toggleForm');
+    }
+})
+
+forRegister.addEventListener('click', () => {
+    forLogin.classList.remove('active');
+    forRegister.classList.add('active');
+    if(registerForm.classList.contains('toggleForm'))
+    {
+        formContainer.style.transform = 'translate(-100%)';
+        formContainer.style.transition = 'transform .5s';
+        registerForm.classList.remove('toggleForm');
+        loginForm.classList.add('toggleForm');
+    }
+})
+
+function pE(e)
+{
+    return document.getElementById(e);
+}
+
+function showForm()
+{
+    document.querySelector('.form-wrapper .card').classList.toggle('show');
+}
