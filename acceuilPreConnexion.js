a1d9d61c24832bb077dc9565046377a6a27c9660
AdelineFernandez <100292061+AdelineFernandez@users.noreply.github.com>
2023-07-15 21:17:13 +0200

    Add files via upload
    Feuilles php et JS associées. Fonctions sur les sélecteurs avec exclusion (Olivier - Adeline -> Equipes et Arbitres)

--- acceuilPreConnexion.js
+++ acceuilPreConnexion.js
@@ -10,11 +10,48 @@ displayform.addEventListener('click', showForm);
 registerButton.addEventListener('click', GlobalVerif);*/
 
 //TEST
+function pE(e)
+{
+    return document.getElementById(e);
+}
+
 const equip2s = pE("equip2selector");
 const equip1s = pE("equip1selector");
+const arbitrePrincS = pE("arbitrePrincSelector");
+const arbitreAss1S = pE("arbitreAssSelector1");
+const arbitreAss2S = pE("arbitreAssSelector2");
 const testDate = pE('inputDate');
 var reliquatEquipe2;
 var reliquatEquipe1;
+let reliquatArbitrePrinc = [];
+let reliquatArbitreAss1 = [];
+let reliquatArbitreAss2 = [];
+var arbitrePrincPrecedent;
+var arbitreAss1Precedent;
+var arbitreAss2Precedent;
+var previousArbitrePrinc="";
+var previousArbitreAss1="";
+var previousArbitreAss2="";
+
+
+function reinitSelector(selectorToModify, reliquatToModify, previousValue)
+{
+    reliquatToModify.forEach((reliquat) => 
+        {
+            console.log(reliquat);
+            if(reliquat.value == previousValue) 
+            {
+                selectorToModify.add(reliquat, selectorToModify[reliquat.value]); // On l'ajoute au sélecteur Arbitre Assistant 1
+            }
+            const index = reliquatToModify.indexOf(reliquat); //Connaître l'index du reliquat en question
+            console.log(index);
+            if (index > -1) //Si indexOf ne trouve rien, il renvoie -1
+            {
+                reliquatToModify.splice(index, 1); //On supprime la case à l'index où on l'a trouvée, le 2nd param indique que l'on retire 1 élém
+            }
+            console.log(reliquatToModify);
+        })
+}
 
 function dateHandler(date)
 {
@@ -25,10 +62,10 @@ function dateHandler(date)
 
 function equip1Handler(equipe)
 {
-    if(equipe == "")
+    if(equipe == "") 
     {
         console.log("remise à zéro");
-        if(reliquatEquipe2 != null)
+        if(reliquatEquipe2 != null) 
         {
             equip2s.add(reliquatEquipe2, equip2s[reliquatEquipe2.value]);
             reliquatEquipe2 = null;
@@ -69,12 +106,211 @@ function equip2Handler(equipe)
         {
             equip1s.add(reliquatEquipe1, equip1s[reliquatEquipe1.value]);
         }
-        reliquatEquipe1 = pE("equip1opt"+equipe);
+        reliquatEquipe1 = pE("equip1opt"+equipe); //on récupère l'équipe sélectionnée dans le select 1 pour la mettre dans le reliquat
         console.log(reliquatEquipe1);
-        equip1s.remove(equipe);
+        equip1s.remove(equipe); //on retire l'équipe sélectionnée du sélect Equipe 1
+        console.log("Vous avez selectionné l'équipe : " + equipe);
 
     }
 }
+
+
+
+function arbitrePrincHandler(arbitre) 
+{
+    
+    reinitSelector(arbitreAss1S, reliquatArbitreAss1, previousArbitrePrinc);
+    reinitSelector(arbitreAss2S, reliquatArbitreAss2, previousArbitrePrinc);
+
+    if (arbitre != "")
+    {
+        var reliquatTemp = pE("arbitreAss1Opt"+arbitre); //On cherche dans notre html l'option du sélect ArbitreAss1 qui contient notre valeur arbitre
+        reliquatArbitreAss1.push(reliquatTemp); //Je le push dans reliquatArbitreAss1
+        reliquatTemp.remove();
+        reliquatTemp = pE("arbitreAss2Opt"+arbitre);
+        reliquatArbitreAss2.push(reliquatTemp);
+        reliquatTemp.remove();
+        console.log(reliquatArbitreAss1); 
+        console.log(reliquatArbitreAss2); 
+        // const arbitreAss1 = pE('arbitreAss1');
+        // arbitreAss1.classList.remove('hidden');
+        console.log("Vous avez sélectionné l'arbitre : " + arbitre);
+    }
+
+    previousArbitrePrinc = arbitre;
+}
+
+
+
+function arbitreAss1Handler(arbitre) 
+{
+     
+    reinitSelector(arbitrePrincS, reliquatArbitrePrinc, previousArbitreAss1);
+    reinitSelector(arbitreAss2S, reliquatArbitreAss2, previousArbitreAss1);
+    
+    if (arbitre != "")
+    {
+        var reliquatTemp = pE("arbitrePrincOpt"+arbitre); //On cherche dans notre html l'option du sélect ArbitreAss1 qui contient notre valeur arbitre
+        reliquatArbitrePrinc.push(reliquatTemp); //Je le push dans reliquatArbitreAss1
+        reliquatTemp.remove();
+        reliquatTemp = pE("arbitreAss2Opt"+arbitre);
+        reliquatArbitreAss2.push(reliquatTemp);
+        reliquatTemp.remove();
+        console.log(reliquatArbitrePrinc); 
+        console.log(reliquatArbitreAss2); 
+
+        // const arbitreAss2 = pE('arbitreAss2');
+        // arbitreAss2.classList.remove('hidden');
+        console.log("Vous avez sélectionné l'arbitre : " + arbitre);
+    }
+
+    previousArbitreAss1 = arbitre;
+}
+
+// function getOptionAss2() 
+// {
+//     var arbitreAss2Selector = document.getElementById("arbitreAss2Selector");
+//     var arbitreAss2Selected = arbitreAss2Selector.value;
+    
+//     console.log("Option précédente : " + arbitreAss2Precedent);
+//     console.log("Option sélectionnée : " + arbitreAss2Selected);
+    
+//     arbitreAss2Precedent = arbitreAss2Selected;
+// }
+
+function arbitreAss2Handler(arbitre) 
+{
+        
+    reinitSelector(arbitrePrincS, reliquatArbitrePrinc, previousArbitreAss2);
+    reinitSelector(arbitreAss1S, reliquatArbitreAss1, previousArbitreAss2);
+    
+    if (arbitre != "")
+    {
+        var reliquatTemp = pE("arbitrePrincOpt"+arbitre); 
+        reliquatArbitrePrinc.push(reliquatTemp); 
+        reliquatTemp.remove();
+        reliquatTemp = pE("arbitreAss1Opt"+arbitre);
+        reliquatArbitreAss1.push(reliquatTemp);
+        reliquatTemp.remove();
+        console.log(reliquatArbitrePrinc); 
+        console.log(reliquatArbitreAss1); 
+
+        console.log("Vous avez sélectionné l'arbitre : " + arbitre);
+    }
+
+    previousArbitreAss2 = arbitre;
+}
+
+// NE FONCTIONNENT PAS CAR ON A 3 POSSIBILITES
+// function arbitrePrincHandler(arbitre)
+// {
+//     if(arbitre == "")
+//     {
+//         console.log("remise à zéro");
+//         if(reliquatArbitreAss1 != null)
+//         {
+//             arbitreAss1S.add(reliquatArbitreAss1, arbitreAss1S[reliquatArbitreAss1.value]);
+//             reliquatArbitreAss1 = null;
+//         }
+//         if(reliquatArbitreAss2 != null)
+//         {
+//             arbitreAss2S.add(reliquatArbitreAss2, arbitreAss2S[reliquatArbitreAss2.value]);
+//             reliquatArbitreAss2 = null;
+//         }
+//     }
+//     else
+//     {
+        
+//         if(reliquatArbitreAss1 != null)
+//         {
+//             arbitreAss1S.add(reliquatArbitreAss1, arbitreAss1S[reliquatArbitreAss1.value]);
+//         }
+//         reliquatArbitreAss1 = pE("arbitreAss1Opt"+arbitre);
+//         console.log(reliquatArbitreAss1);
+//         arbitreAss1S.remove(arbitre);
+//         if(reliquatArbitreAss2 != null)
+//         {
+//             arbitreAss2S.add(reliquatArbitreAss2, arbitreAss2S[reliquatArbitreAss2.value]);
+//         }
+//         reliquatArbitreAss2 = pE("arbitreAss2Opt"+arbitre);
+//         console.log(reliquatArbitreAss2);
+//         arbitreAss2S.remove(arbitre);
+//     }
+// }
+
+// function arbitreAss1Handler(arbitre)
+// {
+//     if(arbitre == "")
+//     {
+//         console.log("remise à zéro");
+//         if(reliquatArbitrePrinc != null)
+//         {
+//             arbitrePrincS.add(reliquatArbitrePrinc, arbitrePrincS[reliquatArbitrePrinc.value]);
+//             reliquatArbitrePrinc = null;
+//         }
+//         if(reliquatAss2 != null)
+//         {
+//             arbitreAss2S.add(reliquatArbitreAss2, arbitreAss2S[reliquatArbitreAss2.value]);
+//             reliquatArbitreAss2 = null;
+//         }
+//     }
+//     else
+//     {
+//         if(reliquatArbitrePrinc != null)
+//         {
+//             arbitrePrincS.add(reliquatArbitrePrinc, arbitrePrincS[reliquatArbitrePrinc.value]);
+//         }
+//         reliquatArbitrePrinc = pE("arbitrePrincOpt"+arbitre);
+//         console.log(reliquatArbitrePrinc);
+//         arbitrePrincS.remove(arbitre);
+//         if(reliquatArbitreAss2 != null)
+//         {
+//             arbitreAss2S.add(reliquatArbitreAss2, arbitreAss2S[reliquatArbitreAss2.value]);
+//         }
+//         reliquatArbitreAss2 = pE("arbitreAss2Opt"+arbitre);
+//         console.log(reliquatArbitreAss2);
+//         arbitreAss2S.remove(arbitre);
+
+//     }
+// }
+
+
+// function arbitreAss2Handler(arbitre)
+// {
+//     if(arbitre == "")
+//     {
+//         console.log("remise à zéro");
+//         if(reliquatArbitrePrinc != null)
+//         {
+//             arbitrePrincS.add(reliquatArbitrePrinc, arbitrePrincS[reliquatArbitrePrinc.value]);
+//             reliquatArbitrePrinc = null;
+//         }
+//         if(reliquatArbitreAss1 != null)
+//         {
+//             arbitreAss1S.add(reliquatArbitreAss1, arbitreAss1S[reliquatArbitreAss1.value]);
+//             reliquatArbitreAss1 = null;
+//         }
+//     }
+//     else
+//     {
+//         if(reliquatArbitrePrinc != null)
+//         {
+//             arbitrePrincS.add(reliquatArbitrePrinc, arbitrePrincS[reliquatArbitrePrinc.value]);
+//         }
+//         reliquatArbitrePrinc = pE("arbitrePrincOpt"+arbitre);
+//         console.log(reliquatArbitrePrinc);
+//         arbitrePrincS.remove(arbitre);
+//         if(reliquatArbitreAss1 != null)
+//         {
+//             arbitreAss1S.add(reliquatArbitreAss1, arbitreAss1S[reliquatArbitreAss1.value]);
+//         }
+//         reliquatArbitreAss1 = pE("arbitreAss1Opt"+arbitre);
+//         console.log(reliquatArbitreAss1);
+//         arbitreAss1S.remove(arbitre);
+
+//     }
+// }
+
 //TEST
 
 
@@ -108,10 +344,7 @@ forRegister.addEventListener('click', () => {
 
 */
 
-function pE(e)
-{
-    return document.getElementById(e);
-}
+
 
 /*function showForm()
 {
--- feuilleMatch.php
+++ feuilleMatch.php
@@ -19,24 +19,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style2.css"/>
+    <link rel="stylesheet" href="style_fixed.css"/>
     <title>Feuille de Match</title>
 </head>
 <body>
     <h1>Création de Feuille de Match</h1>
-    <?php
+    <!-- <?php
     if($db == NULL)
     {
-        //var_dump($db);
         echo "la connexion a la base de données a échoué";
     }
     else{
         echo "bravo connexion effectuée";
-    }  ?>
+    }  ?> -->
 
     <div class="rencontre">
         <form action="CreateFeuilleMatch.php" method="POST">
 
         <input type="date" name="DateRencontre" onchange="dateHandler(value);" id="inputDate" required>
+        
         <div id="equipe1" class="hidden">
             <select id="equip1selector" name="equipe1" onchange="equip1Handler(value);" required><option value="">Equipe N°1</option>
                 <?php 
@@ -52,8 +53,8 @@
                 ?>
             </select>
         </div>
-        <div id="equipe2" class="hidden">
-            
+
+        <div id="equipe2" class="hidden"> 
             <select id="equip2selector" name="equipe2" onchange="equip2Handler(value);" required><option value="">Equipe N°2</option>
             <?php 
                     if(isset($clubs))
@@ -71,49 +72,57 @@
         <div id="lieux">
             <input type="text" name="lieu" placeholder="lieux de rencontre" id="">
         </div>
+        
         <div id="arbitres">
             <div id="arbitrePrinc">
-            <select id="arbitrePrincSelector" name="ArbitreP" onchange="" required><option value="">Arbitre Principal</option>
-            <?php 
-                    if(isset($arbitres))
-                    {
-                        $index = 1;
-                        foreach($arbitres as $arbitre)
-                        {                           
-                            echo "<option id='arbitrePrincSelector".$index."' value=".$arbitre['IdArbitre'].">".$arbitre['NomArbitre']."</option>";
-                            $index++;
+                <select id="arbitrePrincSelector" name="ArbitreP" onchange="arbitrePrincHandler(value);" onchange="getOptionPrinc(value);" required><option value="">Arbitre Principal</option>
+                <?php 
+                        if(isset($arbitres))
+                        {
+                            $index = 1;
+                            foreach($arbitres as $arbitre)
+                            {                           
+                                echo "<option id='arbitrePrincOpt".$index."' value=".$arbitre['IdArbitre'].">".$arbitre['NomArbitre']."</option>";
+                                $index++;
+                            }
                         }
-                    }
-                ?>
-            </select>
+                    ?>
+                </select>
             </div>
-            <div id="arbitreAss">
-            <select id="arbitreAssSelector1" name="ArbitreA1" onchange="" required><option value="">Arbitre Assistant 1</option>
-            <?php 
-                    if(isset($arbitres))
-                    {
-                        $index = 1;
-                        foreach($arbitres as $arbitre)
-                        {                           
-                            echo "<option id='arbitrePrincSelector".$index."' value=".$arbitre['IdArbitre'].">".$arbitre['NomArbitre']."</option>";
-                            $index++;
-                        }
-                    }
-                ?>
-            </select>
-            <select id="arbitreAssSelector2" name="ArbitreA2" onchange="" required><option value="">Arbitre Assistant 2</option>
-            <?php 
-                    if(isset($arbitres))
-                    {
-                        $index = 1;
-                        foreach($arbitres as $arbitre)
-                        {                           
-                            echo "<option id='arbitrePrincSelector".$index."' value=".$arbitre['IdArbitre'].">".$arbitre['NomArbitre']."</option>";
-                            $index++;
-                        }
-                    }
-                ?>
-            </select>
+
+            <div id="arbitresAss">
+                <div id="arbitreAss1">
+                    <select id="arbitreAssSelector1" name="ArbitreA1" onchange="arbitreAss1Handler(value);" onchange="getOptionAss1();"required><option value="">Arbitre Assistant 1</option>
+                    <?php 
+                            if(isset($arbitres))
+                            {
+                                $index = 1;
+                                foreach($arbitres as $arbitre)
+                                {                           
+                                    echo "<option id='arbitreAss1Opt".$index."' value=".$arbitre['IdArbitre'].">".$arbitre['NomArbitre']."</option>";
+                                    $index++;
+                                }
+                            }
+                        ?>
+                    </select>
+                </div>
+
+                <div id="arbitreAss2">
+                    <select id="arbitreAssSelector2" name="ArbitreA2" onchange="arbitreAss2Handler(value);" onchange="getOptionAss2();"required><option value="">Arbitre Assistant 2</option>
+                    <?php 
+                            if(isset($arbitres))
+                            {
+                                $index = 1;
+                                foreach($arbitres as $arbitre)
+                                {                           
+                                    echo "<option id='arbitreAss2Opt".$index."' value=".$arbitre['IdArbitre'].">".$arbitre['NomArbitre']."</option>";
+                                    $index++;
+                                }
+                            }
+                        ?>
+                    </select>
+                </div>
+
             <input type="checkbox" id="scales" name="scales" unchecked>
             <label for="scales">Envoyer un mail aux entraineurs concernés</label>
             <a href = "mailto: abc@example.com">Send Email</a>
@@ -124,4 +133,9 @@
     </div>
     <script src="acceuilPreConnexion.js"></script>
 </body>
+<footer>
+    <?php
+        require("footer.html");
+    ?>
+</footer>
 </html>
