b8d677e45aa855c7bdf1f04525ebc5a4dce5553b
Florian <belkadiflorian1@gmail.com>
2023-08-22 14:28:27 +0200

    ok

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     if(!is_logged())
     {
-        header("location: /auth.php");
+        //header("location: /auth.php");
     }
     $db = connect();
 
@@ -20,9 +20,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="styleIndex.css"/>
+    <link rel="stylesheet" href="style_fixed.css"/>
+    <title>Index</title>
 </head>
+
 <body>
+<header>
+    <div id="headerTop">
+        <div class="header">
+    <ul>
+        <li><a href="#CreateFeuilleMatch">Créer</a></li>
+        <li><a href="#FeuilleMatch">Mes Matchs</a></li>
+        <li><a href="#Tools">Outils</a></li>
+        <li><a href="#Contact">Se déconnecter</a></li>
+    </ul>
+    </div>
+</div>
+</header>
+
+<!-- Carte des Matchs en cours -->
+<section class="card">
+      <h2>Matchs en cours</h2>
+      <div class="match">
+      <?php
+        if(count($matchs) > 0)
+        {
+            var_dump($matchs);
+            echo $matchs[0]["NomEquipe1"];
+            echo $matchs[0]["NomEquipe2"];
+        }
+    ?>
+
+
+<!-- A partir d'ici je ne suis pas sûr que cela soit bon, c'est un test
+
+      </div>
+      <div class="match">
+        <h3>Match 2</h3>
+        <p>Date : 26 juillet 2023</p>
+        <p>Lieu : Stade ABC</p>
+      </div>
+-->
+    </section>
+
+
+<div id="boxMatchs">
+    <div class="matchEnCours">
     <h1>Mes Matchs en cours :</h1>
     <?php
         if(count($matchs) > 0)
@@ -32,5 +76,21 @@
             echo $matchs[0]["NomEquipe2"];
         }
     ?>
+    </div>
+</div>
+
 </body>
+
+<footer>
+    <div id="footer">
+    <?php
+        include("footer.html")
+    ?>
+    </div>
+</footer>
+
+<link rel="stylesheet" href="style_fixed.css"/>
+
+
+
 </html>
